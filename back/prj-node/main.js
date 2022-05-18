8889fc6f996b08ffd64b7731260dbd390d1b7d0a
jeanzacarchuka <jzacarchuka@gmail.com>
2022-05-18 10:37:35 -0300

    versão teste

--- back/prj-node/main.js
+++ back/prj-node/main.js
@@ -1,19 +1,39 @@
-const express = require("express");
-const morgan = require("morgan");
 
+//const express = require("express");
+//const morgan = require('morgan');
+ // const produtos = require ("./produtos.json");
+const mongoose = require("mongoose");
 
-const srv = express();
-srv.use(Morgan('dev'));
-srv.use(express.urlencoded({'extended:': true}));
-srv.use(express.json());
+// var str_connection = "";
+var URL = 'mongodb://localhost:27017/local';
 
-const get = (req, res) => res.send("Servidor Express (GET)");
-const log = () => console.log("Servidor Express rodando em localhost:3000...");
+mongoose.connect(URL);
+const con = mongoose.connection;
 
-const produtosFunc = (req, res) => res.json(produtos);
+con.on('open', function(){
+    console.log('Conectado ao MongoDB!');
+});
 
-srv.get('/', get);
-srv.get('/produtos', produtosFunc);
+con.on('error', function(){
+  console.log('Erro na conexão com o MongoDB!');
+});
 
-srv.listen(3000, log);
+con.on('close', function(){
+  console.log('Desconetado do MongoDB!');
+});
+  
+
+// const srv = express();
+// srv.use(express.urlencoded({'extended:': true}));
+// srv.use(express.json());
+
+// const get = (req, res) => res.send("Servidor Express (GET)");
+// const log = () => console.log("Servidor Express rodando em localhost:3000...");
+
+// const produtosFunc = (req, res) => res.json(produtos);
+
+// srv.get('/', get);
+// srv.get('/produtos', produtosFunc);
+
+// srv.listen(3000, log);
 
